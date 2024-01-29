c5d69ac56b70169ef2c799768f02650d207d01cf
alan.amanco <alan.amanco@sigep.com.br>
2024-01-29 09:49:11 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/grate_places.dart';
 import 'package:great_places/screens/places_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/ultils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +15,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.indigo,
-          secondary: Colors.amber,
+    return ChangeNotifierProvider(
+      create: (context) => GreatePlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.indigo,
+            secondary: Colors.amber,
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          useMaterial3: false,
         ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useMaterial3: false,
+        home: const PlacesListScreen(),
+        routes: {
+          AppRoutes.placeForm: (context) => const PlaceFormScreen(),
+        },
       ),
-      home: const PlacesListScreen(),
-      routes: {
-        AppRoutes.placeForm: (context) => const PlaceFormScreen(),
-      },
     );
   }
 }
